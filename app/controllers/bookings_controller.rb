c136ced9c1540b0ea74261d1b49e55bf5e356427
Marco Montis <marcmontis7@gmail.com>
2020-03-29 19:11:24 +0200

    index, new and create booking logic

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,26 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @user = current_user
+    @booking.user = @user
+    @space = Space.find(params[:space_id])
+    @booking.space = @space
+    @booking.save!
+    redirect_to space_path(@space)
+  end
+
+  def my_reservations
+    @user = current_user
+    @bookings = Booking.where(user_id: current_user)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:check_in, :check_out)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   resources :spaces, only: [:index, :new, :create, :show] do
     resources :bookings, only: [:index, :new, :create]
   end
+
+  resources :bookings do
+    collection do
+      get :my_reservations
+    end
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
