0b5e636e12fd5ad28f0cc82ef9f235cd56741242
Marco Montis <marcmontis7@gmail.com>
2020-03-30 04:19:38 +0200

    geocoding

--- app/models/space.rb
+++ app/models/space.rb
@@ -9,4 +9,11 @@ class Space < ApplicationRecord
   has_one_attached :photo
   # delete all the bookings when a space is deleted
   has_many :bookings, dependent: :destroy
+
+  def full_address
+    [address, city, postcode].compact.join(',')
+  end
+
+  geocoded_by :full_address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
