17f4dedd219d79fa5d5c6fbfb63a33e25f653fe9
Marco Montis <marcmontis7@gmail.com>
2020-04-20 17:07:16 +0200

    additional info checkout summary

--- app/javascript/payments/summary.js
+++ app/javascript/payments/summary.js
@@ -1,8 +1,1 @@
-$(document).ready(function () {
-    $(".content").hide();
-    $(".show_hide").on("click", function () {
-        var txt = $(".content").is(':visible') ? 'Read More'  : 'Read Less';
-        $(".show_hide").text(txt);
-        $(this).next('.content').slideToggle(200);
-    });
-});
+
