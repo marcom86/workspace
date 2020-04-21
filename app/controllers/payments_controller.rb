7a43c2ee4abd9eb1acec53718bc4bb3fe190601b
Marco Montis <marcmontis7@gmail.com>
2020-04-22 00:33:23 +0200

    style details success page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,5 +10,8 @@ class PaymentsController < ApplicationController
 
   def success
     @booking = current_user.bookings.find(params[:booking_id])
+    @days = 1 + (@booking.check_out - @booking.check_in).to_i
+    @service_fee = @booking.space.price * @days * 0.15
+    @total_price = (@booking.space.price * @days) + @service_fee
   end
 end
