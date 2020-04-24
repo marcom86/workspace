a549e70c4f25bdb3402811707d9f867e4fdac01a
Marco Montis <marcmontis7@gmail.com>
2020-04-24 05:28:19 +0200

    add top spaces to home

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,23 +1,16 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("payments/summary")
+
 
 import "bootstrap";
 
 import { initMapbox } from '../plugins/init_mapbox';
 import { scroll } from '../pages/scroll';
 
+
 initMapbox();
 scroll();
 
-changeBadgeColor();
 
 
-function changeBadgeColor(){
-  const badge = document.getElementById('booking-badge');
-
-  badge.classList.remove("badge-warning");
-  badge.classList.add("badge-primary");
-
-}
--- app/javascript/payments/stripe.js
+++
@@ -1,9 +0,0 @@
-// function changeBadgeColor(){
-//   const badge = document.getElementById('booking-badge');
-
-//   badge.classList.remove("badge-warning");
-//   badge.classList.add("badge-primary");
-
-// }
-
-
