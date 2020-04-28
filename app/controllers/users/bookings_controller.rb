4eab5c089c7069855a1e94753d17e207a8b2fabd
Marco Montis <marcmontis7@gmail.com>
2020-04-29 00:26:56 +0200

    homepage responsive

--- app/controllers/users/bookings_controller.rb
+++ app/controllers/users/bookings_controller.rb
@@ -1,5 +1,9 @@
 class Users::BookingsController < ApplicationController
   def index
-    @bookings = current_user.bookings.order(created_at: params[:sort])
+    @bookings = current_user.bookings
+
+    if params[:sort].present?
+      @bookings = current_user.bookings.order(created_at: params[:sort])
+    end
   end
 end
